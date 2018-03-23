cee806f6296986aebc673ccee1a23dfa131c5b7c
tconns <thanhcongns94@gmail.com>
2018-03-23 09:19:20 +0700

    Update function module android

--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-'use strict'
+"use strict";
 
-const { NativeModules } = require('react-native');
+const { NativeModules } = require("react-native");
 const { ImagePickerManager } = NativeModules;
 
 const DEFAULT_OPTIONS = {
-  title: 'Select a Photo',
-  cancelButtonTitle: 'Cancel',
-  takePhotoButtonTitle: 'Take Photo…',
-  chooseFromLibraryButtonTitle: 'Choose from Library…',
+  title: "Select a Photo",
+  cancelButtonTitle: "Cancel",
+  takePhotoButtonTitle: "Take Photo…",
+  chooseFromLibraryButtonTitle: "Choose from Library…",
   quality: 1.0,
   allowsEditing: false,
   permissionDenied: {
-    title: 'Permission denied',
-    text: 'To be able to take pictures with your camera and choose images from your library.',
-    reTryTitle: 're-try',
-    okTitle: 'I\'m sure',
+    title: "Permission denied",
+    text:
+      "To be able to take pictures with your camera and choose images from your library.",
+    reTryTitle: "re-try",
+    okTitle: "I'm sure"
   }
 };
 
 module.exports = {
   ...ImagePickerManager,
-  showImagePicker: function showImagePicker(options, callback) {
-    if (typeof options === 'function') {
+  getImageCamera: function getImageCamera(options, callback) {
+    if (typeof options === "function") {
       callback = options;
       options = {};
     }
-    return ImagePickerManager.showImagePicker({...DEFAULT_OPTIONS, ...options}, callback)
+    return ImagePickerManager.getImageCamera(
+      { ...DEFAULT_OPTIONS, ...options },
+      callback
+    );
+  },
+  getImageLibrary: function getImageLibrary(options, callback) {
+    if (typeof options === "function") {
+      callback = options;
+      options = {};
+    }
+    return ImagePickerManager.getImageLibrary(
+      { ...DEFAULT_OPTIONS, ...options },
+      callback
+    );
+  },
+  showImagePicker: function showImagePicker(options, callback) {
+    if (typeof options === "function") {
+      callback = options;
+      options = {};
+    }
+    return ImagePickerManager.showImagePicker(
+      { ...DEFAULT_OPTIONS, ...options },
+      callback
+    );
   }
-}
+};
